7d38bf7c9e404e98f38dee5a3bcb62c922cf57d4
Aurelily <aurelie.preaud@gmail.com>
2024-05-11 17:17:31 +0200

    Jour 01 job 13 OK

--- jour01/job13/script.js
+++ jour01/job13/script.js
@@ -1,6 +1,6 @@
-import { createServer } from 'node:http';
-import { readFile } from 'node:fs';
-import { resolve } from 'node:path';
+import { createServer } from 'http';
+import { readFile } from 'fs';
+import { resolve } from 'path';
 
 const server = createServer((req, res) => {
   if (req.url === '/') {
@@ -16,14 +16,27 @@ const server = createServer((req, res) => {
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(data);
     });
+  } else if (req.url === '/about') {
+    // Lire le fichier about.html
+    readFile(resolve('about.html'), (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Erreur interne du serveur');
+        return;
+      }
+
+      // Envoyer le contenu du fichier HTML
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
   } else {
-    // Si l'URL demandée n'est pas la racine, renvoyer une erreur 404
+    // Si l'URL demandée n'est pas la racine ni /about, renvoyer une erreur 404
     res.writeHead(404);
     res.end('Page non trouvée');
   }
 });
 
-// starts a simple http server locally on port 3000
+// Démarre un serveur HTTP simple localement sur le port 3000
 server.listen(3000, 'localhost', () => {
   console.log('Listening on 127.0.0.1:3000');
 });
