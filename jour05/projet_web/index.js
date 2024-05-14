6a4694c6dc4d6f5c00b96c5deb2cf8b6d6d83fc6
Aurelily <aurelie.preaud@gmail.com>
2024-05-14 16:10:07 +0200

    Jour05 job05 DONE

--- jour05/projet_web/index.js
+++ jour05/projet_web/index.js
@@ -1,15 +1,24 @@
 const express = require('express')
+// NOTE : J'ai aussi installé le module nodemon pour ne pas avoir à relancer mon serveur à chaque fois que je fais une modif : npm install -g nodemon
+// Du coup je lance mon appli en faisant nodemon index.js plutôt que node index.js
+
+const path = require('path')
 const app = express()
 // NOTE : je ne peux pas utiler le port 80 du sujet car sur mon système d'exploitation il est utilisé en permanence par Apache
 // Du coup j'ai choisi un port non privilégié : 3000
 const port = 3000 
 
+
+// Définir le chemin vers le dossier views où se trouvent les fichiers HTML
+const publicPath = path.join(__dirname, 'views')
+
+
 app.get('/', (req, res) => {
-  res.send('HOME PAGE')
+  res.sendFile(path.join(publicPath, '/index.html'))
 })
 
 app.get('/about', (req, res) => {
-  res.send('ABOUT PAGE')
+  res.sendFile(path.join(publicPath, '/about.html'))
 })
 
 app.listen(port, () => {
